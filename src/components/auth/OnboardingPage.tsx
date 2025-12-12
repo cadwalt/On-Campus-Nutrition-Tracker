c641d893082aed4b2bed48f163d2e1748a04f61e
Cadence Walton <cadencewalton@gmail.com>
2025-12-12 10:05:39 -0600

    Revert "Merge pull request #64 from cadwalt/Weight-Tracking"
    This reverts commit 31df0ecbf7d151c0238699cd8ba7efcee08a4634, reversing
    changes made to feebca592467cb6a46347130e60977ec10b63aee.

--- src/components/auth/OnboardingPage.tsx
+++ src/components/auth/OnboardingPage.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Example: LoginForm.tsx
 import React, {  } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -31,38 +30,4 @@ const LoginForm: React.FC = () => {
   );
 };
 
-=======
-// Example: LoginForm.tsx
-import React, {  } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-const LoginForm: React.FC = () => {
-  const navigate = useNavigate();
-  // ...existing login logic...
-
-  return (
-    <div className="auth-form">
-      <h3 className="auth-title">Sign In</h3>
-      <form className="auth-form-content">
-        {/* ...login fields... */}
-        <button type="submit" className="auth-button">
-          Sign In
-        </button>
-      </form>
-      {/* Add link to sign up page */}
-      <div className="auth-switch">
-        <span>Don’t have an account?</span>
-        <button
-          type="button"
-          className="auth-link"
-          onClick={() => navigate('/signup')}
-        >
-          Create one
-        </button>
-      </div>
-    </div>
-  );
-};
-
->>>>>>> 3449604f23503c51d893151942e46f034bb45a8d
 export default LoginForm;
--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 export { default as SignInForm } from './SignInForm';
 export { default as SignUpForm } from './SignUpForm';
-=======
-export { default as SignInForm } from './SignInForm';
-export { default as SignUpForm } from './SignUpForm';
->>>>>>> 3449604f23503c51d893151942e46f034bb45a8d
